7edc639d9a3ba49f83d3e9881feb4fc431cbd055
matsuki <matsuki@cloudpack.jp>
2022-02-16 06:43:24 +0000

    modify s3 bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/clp-test/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "Gaurav"
+  bucket_prefix = "gaurav-"
   acl    = "private"
 
   versioning = {
