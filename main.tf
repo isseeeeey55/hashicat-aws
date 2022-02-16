a26572e93a93ac6d42d7b320d18135651908681d
matsuki <matsuki@cloudpack.jp>
2022-02-16 06:16:31 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "dev-ops"
+    Department = "devops",
+    Billable = "true"
   }
 }
 
